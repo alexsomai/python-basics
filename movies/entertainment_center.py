de4212898503775a74cb2004d48b2d700d046da4
Alexandru Somai <somai.alexandru@gmail.com>
2017-03-14 17:56:35 +0200

    Dummy movie website

---
+++ movies/entertainment_center.py
@@ -0,0 +1,13 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy story", "A story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar", "A marine on an alien planet",
+                     "http://www.movieposter.com/posters/archive/main/98/MPW-49246",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+movies = [toy_story, avatar]
+fresh_tomatoes.open_movies_page(movies)
